8559296239a91874c01c53e7057ba7b7f0138f8a
AyberkAksamaz <76598712+AyberkAksamaz@users.noreply.github.com>
2021-10-22 00:57:01 +0300

    Delete ECommerceProject directory

--- ECommerceProject/src/business/abstracts/UserService.java
+++
@@ -1,9 +0,0 @@
-package business.abstracts;
-
-import entities.concretes.User;
-
-public interface UserService {
-
-	void signin(User user);
-	void signup(User user);
-}
--- ECommerceProject/src/core/LoginService.java
+++
@@ -1,9 +0,0 @@
-package core;
-
-import entities.concretes.User;
-
-public interface LoginService {
-
-	void signUp(User user);
-	void signIn(User user);
-}
